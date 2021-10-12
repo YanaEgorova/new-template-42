682819d1b0eb792465714a642bdcfcb6b041e4e4
Yana <yanaegorova69.3@gmail.com>
2021-10-12 23:03:24 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,155 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Sleep Mask',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Natural Silk Sleep Mask, Super-Smooth and Soft Eye Mask with Adjustable Strap, Blindfold, Perfect Blocks Light, Pressure Free for A Full Night\'s Sleep.',
+            },
+            {
+                text: '100% skin friendly natural lightweight breathable material, silk fabric. Super light weight ,Ultra soft and comfortable.',
+            },
+            {
+                text: 'Blocks out light design to ensure complete darkness, offer you a Mid-night\'s environment. Put on it and enjoy your "sweet dreams" whether at home, in a hotel or on the plane.',
+            },
+            {
+                text: 'Additional earplugs help to prevent noise around you, work together with the mask to give you a more comfortable relax environment- Quiet and darkness.',
+            },
+            {
+                text: 'ONE SIZE FOR ALL: Gentle, easy-to-adjust headband won\'t tangle hair like masks that use velcro. Suitable for men and women.',
+            },
+            {
+                text: 'A UNIQUE GIFT: This pure silk sleep mask can be a special gift for your parents, children, husband, wife, elders and friends. We believe our product after first-hand experience.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Ironing Board',
+        price: 29.99,
+        image: './img/img2.png',
+        description: [
+            {
+                title: 'Ideal for Compact Spaces',
+                text: 'The perfect table top ironing board—ideal for travel and compact spaces.',
+            },
+            {
+                title: 'Features',
+                text: 'A sturdy chrome-wire iron rest.',
+            },
+            {
+                title: 'Includes',
+                text: 'A quality cotton cover.',
+            },
+            {
+                title: 'Measures',
+                text: '32-inches x 12-inches when set up and stands 7-inches tall.',
+            },
+            {
+                title: 'Easy to Store',
+                text: 'Legs fold under and board folds to 16-inches x 12-inches for easy storage.',
+            },
+            {
+                title: 'Fit type',
+                text: 'Universal Fit.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Noise Machine',
+        price: 64.90,
+        image: './img/img3.png',
+        description: [
+            {
+                text: '2 speed options for adjustable tone and volume creates a personalized sound environment, excellently masking noise for better sleep.',
+            },
+            {
+                text: 'Powered by a 6 foot 120V AC plug, just plug it in, flip the switch to high or low, then rotate the cap and collar to find the best sound for you.',
+            },
+            {
+                text: 'Modernized look; fits anywhere and blends unobtrusively into surroundings.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Laser Rangefinders',
+        price: 98.90,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'The laser 1000 features an intuitive platform with angle compensation; perfect for bowhunters and range-day lovers. Range targets down to five yards out, with the ability to stretch out to 1,000 yards if need be.',
+            },
+            {
+                text: 'The primary HCD mode displays an angle compensated distance that is ideal for the majority of hunters and shooters. An advanced LOS mode provides the option to calculate long distance, high angle shots with increased precision.',
+            },
+            {
+                text: 'A scan feature gives continuous range readings to pan across a landscape or track a moving target. All modes can be set to read in yards or meters.',
+            },
+            {
+                text: 'The diopter located on the eyepiece adjusts for differences in a user\'s eyes. Added 6x magnification brings targets in for a closer look and more accurate ranging.',
+            },
+            {
+                text: 'Fully multi-coated optics enhance light transmission in low light settings. Waterproof and shockproof performance keep you in the hunt no matter what the conditions.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Expandable Luggage',
+        price: 119.98,
+        image: './img/img5.png',
+        description: [
+            {
+                text: '21 inches  luggage maximizes your packing power and meets most carry on size restrictions for those traveling domestically and looking to stay light.',
+            },
+            {
+                text: 'Packing dimensions: 20" x 14" x 9", Overall dimensions: 22” x 15”x 9.5”, Weight: 8.2 pounds.',
+            },
+            {
+                text: 'Book opening case with mesh divider and Cross straps in main compartment with a zipped modesty pocket.',
+            },
+            {
+                text: '4, multi directional oversized spinner wheels for effortless mobility, re engineered lightweight;Volume : 44 Litres.',
+            },
+            {
+                text: '1.5" Expansion to allow you to pack more items and compression keeps clothing neatly pressed.',
+            },
+            {
+                text: 'Push button locking handles provides easy maneuverability when extended out from the suitcase, and stores neatly inside when not in use.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png', './img/img5-4.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Security Safe',
+        price: 148.00,
+        image: './img/img6.png',
+        description: [
+            {
+                text: '1.8-cubic foot home safe for storing important documents, jewelry, and other valuable property.',
+            },
+            {
+                text: 'Programmable electronic keypad ensures secure, easy operation; back-up key for emergency use; AA batteries required.',
+            },
+            {
+                text: 'Heavy-duty carbon-steel construction (8-gauge steel door and 14-gauge steel body); 2 live-door bolts and pry-resistant concealed hinges for superior security.',
+            },
+            {
+                text: 'Includes pre-drilled mounting holes and hardware for floor and wall mounting; weighs 37.5 pounds.',
+            },
+            {
+                text: 'Exterior measures 13.8 x 13 x 19.7 inches (L x W x H); interior measures 13.6 x 10.6 x 19.6 inches.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png','./img/img6-3.png','./img/img6-4.png','./img/img6-5.png'],
+    },
+]
